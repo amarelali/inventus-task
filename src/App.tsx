b2c6cf54029851d10862203a843375b284974b30
amarelali <amar.elali@isae.edu.lb>
2024-04-24 20:35:50 +0300

    time functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Status from "./components/SVG/Status";
 import Date from "./components/SVG/Date";
 import Close from "./components/SVG/Close";
 import DetailsMenu from "./components/DetailsMenu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./components/ui/Button";
 import { dataDetailsMenu } from "./components/contants";
 import { controleButtonStyle } from "./components/contants/styles";
@@ -16,14 +16,51 @@ import { controleButtonStyle } from "./components/contants/styles";
 function App() {
   const [covers, setEditedCovers] = useState<number>(0);
   const [duration, setDuration] = useState<number>(1);
-
+  const [hour, setHour] = useState<number>(12);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [finalTime, setFinalTime] = useState<string>("12:00 AM");
+  const [isAM, setIsAM] = useState<boolean>(true);
   const [isOpenIndex, setOpenIndex] = useState(-1);
+  useEffect(() => {
+    setFinalTime(formatTime(hour, minutes));
+  }, [hour, minutes, isAM]);
+
   const handleOpen = (index: number) => {
     setOpenIndex((prevIndex) => (prevIndex === index ? -1 : index));
   };
+  const editTimeIncrement = () => {
+    if (minutes >= 59) {
+      setHour((prev) => (prev === 12 ? 1 : prev + 1));
+      setMinutes(0);
+    } else {
+      setMinutes((prev) => (prev === 59 ? 0 : prev + 1));
+    }
+    if (hour === 11 && minutes === 59) {
+      setIsAM((prevIsAM) => !prevIsAM);
+    }
+  };
+  const editTimeDecrement = () => {
+    if (minutes === 0) {
+      setHour((prev) => (prev === 1 ? 12 : prev - 1));
+      setMinutes(59);
+    } else {
+      setMinutes((prev) => prev - 1);
+    }
+
+    if (hour === 12 && minutes === 0) {
+      setIsAM((prevIsAM) => !prevIsAM);
+    }
+  };
+  const formatTime = (hour: number, minutes: number): string => {
+    const term = isAM ? "AM" : "PM";
+    if (minutes < 10) {
+      return `${hour}:0${minutes} ${term}`;
+    }
+    return `${hour}:${minutes} ${term}`;
+  };
   return (
     <>
-      <ul className="bg-white h-screen font-semibold font-sans w-1/2 md:w-1/4 relative">
+      <ul className="bg-white h-screen font-semibold font-sans w-[57%] md:w-1/4 relative">
         <MenuItems
           icon={<Date />}
           title="Sat, January 20"
@@ -32,14 +69,32 @@ function App() {
           isOpen={isOpenIndex === 0}
           onOpen={() => handleOpen(0)}
         ></MenuItems>
-        <MenuItems
-          icon={<Time />}
-          title="03:15 PM"
-          iconTitle="Time"
-          className="p-3"
-          isOpen={isOpenIndex === 1}
-          onOpen={() => handleOpen(1)}
-        ></MenuItems>
+        <li>
+          <span
+            className={`p-3 flex items-center text-gray-900 dark:text-white hover:bg-[#dbf3e3] hover:text-black dark:hover:bg-gray-700 group`}
+          >
+            <span className="text-[13px] flex items-center gap-1 text-[#aaaaaa]">
+              <Time /> {"Time"}
+            </span>
+            <span className="flex flex-3 justify-between items-center ms-3 whitespace-nowrap w-full">
+              {finalTime}
+              <span className="flex gap-3">
+                <Button
+                  className={controleButtonStyle}
+                  onClick={editTimeDecrement}
+                >
+                  -
+                </Button>
+                <Button
+                  className={controleButtonStyle}
+                  onClick={editTimeIncrement}
+                >
+                  +
+                </Button>
+              </span>
+            </span>
+          </span>
+        </li>
         <li>
           <span
             className={`p-3 flex items-center text-gray-900 dark:text-white hover:bg-[#dbf3e3] hover:text-black dark:hover:bg-gray-700 group`}
