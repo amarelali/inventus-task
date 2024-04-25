dde63272770cd08e6df8629ca3322479fc71dafb
amarelali <amar.elali@isae.edu.lb>
2024-04-25 09:14:35 +0300

    add toggle

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { useEffect, useState } from "react";
 import Button from "./components/ui/Button";
 import { dataDetailsMenu } from "./components/contants";
 import { controleButtonStyle } from "./components/contants/styles";
+import Toggle from "./components/ui/Toggle";
 
 function App() {
   const [covers, setEditedCovers] = useState<number>(0);
@@ -183,6 +184,16 @@ function App() {
             ))}
           </span>
         </MenuItems>
+        <li>
+          <span
+            className={`p-2 flex items-center text-gray-900 dark:text-white hover:bg-[#dbf3e3] hover:text-black dark:hover:bg-gray-700 group`}
+          >
+            <span className="flex flex-3 gap-1 justify-between items-center ms-3 whitespace-nowrap w-full text-balance flex-wrap lg:flex-nowrap">
+              {"Send Reservation Requested Message"}
+              <Toggle />
+            </span>
+          </span>
+        </li>
       </ul>
     </>
   );
---
+++ src/components/ui/Toggle.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { Switch } from '@headlessui/react'
+
+const Toggle =()=> {
+  const [enabled, setEnabled] = useState(true)
+
+  return (
+    <Switch
+      checked={enabled}
+      onChange={setEnabled}
+      className={`${
+        enabled ? 'bg-[#26b464]' : 'bg-gray-400'
+      } relative inline-flex h-6 w-11 items-center rounded-full`}
+    >
+      <span className="sr-only">Enable notifications</span>
+      <span
+        className={`${
+          enabled ? 'translate-x-6' : 'translate-x-1'
+        } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+      />
+    </Switch>
+  )
+}
+
+export default Toggle;
