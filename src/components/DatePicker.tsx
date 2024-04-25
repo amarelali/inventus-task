9c80720ea440dd59e0b390d76bf69193644b6014
amarelali <amar.elali@isae.edu.lb>
2024-04-25 16:29:37 +0300

    fix date picker

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,51 +1,31 @@
-import  { useState } from "react";
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-interface IProps{
-    setFunction:(date:string)=>void;
+interface IProps {
+  setFunction: (date: string) => void;
 }
-const DatePickerComponent = ({setFunction}:IProps) => {
+const DatePickerComponent = ({ setFunction }: IProps) => {
   const [startDate, setStartDate] = useState<Date>(new Date());
-  function getDayName(dayNumber: number) {
-    const date = new Date(dayNumber);
-    const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-    const dayIndex = date.getDay();
-    const dayName = dayNames[dayIndex];
-    return dayName;
-  }
-  function getMonthName(monthIndex: number): string {
-    const monthNames = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    // Get the name of the month from the monthNames array
-    const monthName = monthNames[monthIndex];
 
-    return monthName;
-  }
   const formatDate = (date: Date) => {
+    console.log(date);
     setFunction(
-      `${getDayName(date.getDay())}, ${getMonthName(
-        date.getMonth()
-      )} ${date.getDay()}`
+      `${date.toLocaleDateString("en-US", {
+        weekday: "short",
+      })}, ${date.toLocaleDateString("en-US", {
+        month: "long",
+      })} ${date.getUTCDate()}`
     );
   };
   return (
     <>
-      <DatePicker selected={startDate} onChange={(date:Date) =>{
-         formatDate(date);
-         setStartDate(date);
-      }} />
+      <DatePicker
+        selected={startDate}
+        onChange={(date: Date) => {
+          formatDate(date);
+          setStartDate(date);
+        }}
+      />
     </>
   );
 };
